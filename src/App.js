56d69c22118ee9819cc5d2d466db1da917d8376b
Florent Creton <slimtouflo@gmail.com>
2023-12-21 17:11:44 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import members from "./members.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   // Initialisation du state de notre composant App
+  //   // Le state se résume à une propriété de notre classe App
+  //   // En React la propriété state sera toujours un objet
+  //   this.state = {}
+  // }
+  // équivalent à (depuis ES2019)
+  state = {
+    // members: members
+    // simplification possible car la clé et la valeur portent le même nom
+    members
+  };
+
+  // La méthode render sera automatiquement appelée lorsque le composant App sera interrogé dans du JSX sous la forme suivante : <App />
+  render() {
+    const listMembers = this.state.members.map((member) => {
+      return (
+        <li>{member.name}</li>
+      )
+    })
+
+    return (
+      <div className="container">
+        <h1>Hello React</h1>
+        <p>Liste des membres de la formation :</p>
+        <ul>
+          {/* <li>{this.state.members[0].name}</li> */}
+          {listMembers}
+        </ul>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/members.js
@@ -0,0 +1,20 @@
+const members = [
+    {
+        id: 100,
+        name: "Mohamed"
+    },
+    {
+        id: 101,
+        name: "Rémi"
+    },
+    {
+        id: 102,
+        name: "Aurélie"
+    },
+    {
+        id: 103,
+        name: "Mathieu"
+    }
+];
+
+export default members;
